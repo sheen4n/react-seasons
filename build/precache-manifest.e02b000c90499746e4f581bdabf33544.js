933760e360d78f304586d2f7a82efbe601754ff4
sheen4n <sheen.an.goh@gmail.com>
2020-03-05 14:22:24 +0800

    Added Build

---
+++ build/precache-manifest.e02b000c90499746e4f581bdabf33544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e895fc67df260b59f241e96c1701cc9a",
+    "url": "/react-seasons/index.html"
+  },
+  {
+    "revision": "3de007c56386951dd26d",
+    "url": "/react-seasons/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "c24b1b304a4bc9624974",
+    "url": "/react-seasons/static/js/2.28885a1b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-seasons/static/js/2.28885a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de007c56386951dd26d",
+    "url": "/react-seasons/static/js/main.a7184584.chunk.js"
+  },
+  {
+    "revision": "0111a103af5926d42331",
+    "url": "/react-seasons/static/js/runtime-main.4adb92a8.js"
+  }
+]);
