a83b371e70c870de37a3fcc66a8c18901fb179bd
sheen4n <sheen.an.goh@gmail.com>
2020-03-05 14:09:49 +0800

    Stable Commit to GH Pages

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr its cold!',
+    iconName: 'snowflake'
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) return lat > 0 ? 'summer' : 'winter';
+  return lat > 0 ? 'winter' : 'summer';
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive icon ${iconName}`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive icon ${iconName}`}></i>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
